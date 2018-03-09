dec0f1b933efd6aafca5a4d5b3358431ce3269a3
Bastian Schon <serious.haircut@gmail.com>
2018-03-09 13:57:51 +0100

    register dlx queue in beetle as well to be able to purge (avoid artifacts in rabbitmq in between different attempt runs

--- examples/attemts_with_dlx_and_per_message_ttl.rb
+++ examples/attemts_with_dlx_and_per_message_ttl.rb
@@ -12,43 +12,46 @@ Beetle.config.logger.level = Logger::INFO
 # setup client with dead lettering enabled
 config = Beetle::Configuration.new
 config.dead_lettering_enabled = true
-config.dead_lettering_msg_ttl = 8000 # millis - works as 'global/queue-bases' message-ttl
+config.dead_lettering_msg_ttl = 6000
 
 client = Beetle::Client.new(config)
 client.register_queue(:test)
 client.register_message(:test)
+
 client.register_message(:test_dead_letter) # to allow for directly publishing to this queue (just proof of concept)
 
 # purge the test queue
 client.purge(:test)
+client.register_queue(:test_dead_letter) # only registered to use purge (the dead letter queues are not registered with beetle)
+client.purge(:test_dead_letter)
 # empty the dedup store
 client.deduplication_store.flushdb
 
 # setup our counter
 $completed = 0
-$expected = 2
+$expected = 3
 # store the start time
 $start_time = Time.now.to_f
 
 # declare a handler class for message processing
 class Handler < Beetle::Handler
   def process
-    logger.info "Processed after #{Time.now - $start_time}s: #{message.data}"
+    logger.info "Processed after #{Time.now.to_f - $start_time}s: #{message.data}"
     $completed += 1
   end
 end
 client.register_handler(:test, Handler)
 
 # publish messages directly into the queue whose dlx setup points backwards to the target queue 'test'
-client.publish(:test_dead_letter, 'early', expiration: 4000)  # processed after 4 seconds
-client.publish(:test_dead_letter, 'later', expiration: 12000) # processed after 8 seconds (global/queue based expiration)
-puts "published 2 test messages"
+client.publish(:test_dead_letter, 'earliest', expiration: 2000)                # processed after 2 seconds
+client.publish(:test_dead_letter, 'latest', expiration: 8000)                  # processed after 8 seconds
+client.publish(:test_dead_letter, 'should be in the middle', expiration: 5000) # processed after 8 seconds
+puts "published 3 test messages"
 
 # start the listening loop
-client.listen do
+client.listen_queues([:test]) do
   # catch INT-signal and stop listening
   trap("INT") { client.stop_listening }
-  # we're adding a periodic timer to check whether all 10 messages have been processed without exceptions
   timer = EM.add_periodic_timer(1) do
     if $completed == $expected
       timer.cancel
